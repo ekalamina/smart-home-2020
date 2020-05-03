7a2a56615888849585db330b39f5597bd4367d83
Каламина Эльвира <elvira.kalamina@mail.ru>
2020-05-03 22:29:53 +0300

    Beans, sensorEventsManager

--- src/main/java/ru/sbt/mipt/oop/SpringApplication.java
+++ src/main/java/ru/sbt/mipt/oop/SpringApplication.java
@@ -1,5 +1,6 @@
 package ru.sbt.mipt.oop;
 
+import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -7,6 +8,6 @@ public class SpringApplication {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        applicationContext.getBean(EventsCycle.class).runCycle();
+        applicationContext.getBean(SensorEventsManager.class).start();
     }
 }
--- src/main/java/ru/sbt/mipt/oop/SpringConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/SpringConfiguration.java
@@ -1,25 +1,26 @@
 package ru.sbt.mipt.oop;
 
+import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
-import ru.sbt.mipt.oop.handlers.EventHandler;
+import ru.sbt.mipt.oop.adapters.EventHandlerAdapter;
+import ru.sbt.mipt.oop.decorators.AlarmDecorator;
+import ru.sbt.mipt.oop.decorators.SmsDecorator;
+import ru.sbt.mipt.oop.handlers.*;
 
+import java.util.List;
+
+
+@Configuration
 public class SpringConfiguration {
 
-    private EventsCycles eventsCycles;
     private SmartHome smartHome;
     private HomeInitializer homeInitializer;
 
-    public SpringConfiguration(){
+    public SpringConfiguration() {
         this.homeInitializer = new HomeJsonInitializer();
         this.smartHome = homeInitializer.initializeHome();
-        this.eventsCycles = new EventsCycle();
-    }
-    @Bean
-    @Description("Cycle")
-    EventsCycles getEventsCycle() {
-        return this.eventsCycles;
     }
 
     @Bean
@@ -28,4 +29,30 @@ public class SpringConfiguration {
         return this.smartHome;
     }
 
+    @Bean
+    EventHandler eventAlarmHandler() {
+        return new SmsDecorator(new EventAlarmHandler());
+    }
+
+    @Bean
+    EventHandler eventLightHandler() {
+        return new AlarmDecorator(new EventLightHandler());
+    }
+
+    @Bean
+    EventHandler eventDoorHandler() {
+        return new AlarmDecorator(new EventDoorHandler());
+    }
+
+
+    @Bean
+    public SensorEventsManager sensorEventsManager(List<EventHandler> eventHandlers) {
+        SensorEventsManager sensorEventsManager = new SensorEventsManager();
+
+        eventHandlers.forEach(eventHandler -> {
+            sensorEventsManager.registerEventHandler(new EventHandlerAdapter(smartHome, eventHandler));
+        });
+
+        return sensorEventsManager;
+    }
 }
